b8d97a98bb159f912c149ccf0f6a87fb324ef231
jmax2020 <jmax2020@mail.ru>
2023-02-02 00:51:55 +0300

    5. PsqlStore [#285209 #386629]

--- src/main/java/ru/job4j/grabber/PsqlStore.java
+++ src/main/java/ru/job4j/grabber/PsqlStore.java
@@ -2,11 +2,8 @@ package ru.job4j.grabber;
 
 import ru.job4j.grabber.utils.HabrCareerDateTimeParser;
 
-import java.io.IOException;
 import java.io.InputStream;
 import java.sql.*;
-import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Properties;
@@ -33,19 +30,30 @@ public class PsqlStore implements Store {
 
     @Override
     public void save(Post post) {
-        try  {
-            PreparedStatement ps = cnn.prepareStatement(
-                    String.format("insert into post(name, text, link, created) %s %s",
-                            "values (?, ?, ?, ?) on conflict (link)",
-                            "do update set name = Excluded.name, text = excluded.text"));
+        try (PreparedStatement ps = cnn.prepareStatement(
+                String.format("insert into post(name, text, link, created) %s %s",
+                        "values (?, ?, ?, ?) on conflict (link)",
+                        "do nothing"), Statement.RETURN_GENERATED_KEYS)) {
             ps.setString(1, post.getTitle());
             ps.setString(2, post.getDescription());
             ps.setString(3, post.getLink());
             ps.setTimestamp(4, Timestamp.valueOf(post.getCreated()));
             ps.execute();
+            ResultSet rs = ps.getGeneratedKeys();
+            if (rs.next()) {
+                post.setId(rs.getInt(1));
+            }
         } catch (SQLException e) {
             throw new RuntimeException(e);
         }
+    }
+
+    private Post getPost(ResultSet rs) throws SQLException {
+        return new Post(rs.getInt("id"),
+                rs.getString("name"),
+                rs.getString("link"),
+                rs.getString("text"),
+                rs.getTimestamp("created").toLocalDateTime());
     }
 
     @Override
@@ -54,14 +62,7 @@ public class PsqlStore implements Store {
         try (Statement st = cnn.createStatement()) {
             ResultSet res = st.executeQuery("select * from post");
             while (res.next()) {
-                DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
-                LocalDateTime ldt = LocalDateTime.parse(res.getString(5), dtf);
-                Post post = new Post(res.getInt(1),
-                      res.getString(2),
-                      res.getString(3),
-                      res.getString(4),
-                      ldt);
-               posts.add(post);
+               posts.add(getPost(res));
            }
         } catch (SQLException e) {
             throw new RuntimeException(e);
@@ -72,17 +73,13 @@ public class PsqlStore implements Store {
     @Override
     public Post findById(int id) {
         Post rsl = null;
-        String query = String.format("select * from post where id = %s", id);
-        try (Statement st = cnn.createStatement()) {
-            ResultSet res = st.executeQuery(query);
-            if (res.next()) {
-                DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
-                LocalDateTime ld = LocalDateTime.parse(res.getString(5), dtf);
-                rsl = new Post(res.getInt(1),
-                        res.getString(2),
-                        res.getString(3),
-                        res.getString(4),
-                        ld);
+        try (PreparedStatement ps = cnn.prepareStatement(
+                "select * from post where id = ?")) {
+            ps.setInt(1, id);
+            try (ResultSet rs = ps.executeQuery()) {
+                if (rs.next()) {
+                    rsl = getPost(rs);
+                }
             }
         } catch (SQLException e) {
             throw new RuntimeException(e);
@@ -102,7 +99,7 @@ public class PsqlStore implements Store {
         List<Post> list;
         try (InputStream in = PsqlStore.class.getClassLoader().getResourceAsStream("PsqlStore.properties")) {
             cfg.load(in);
-            PsqlStore psql = new PsqlStore(cfg);
+            try (PsqlStore psql = new PsqlStore(cfg)) {
             HabrCareerParse hcp = new HabrCareerParse(new HabrCareerDateTimeParser());
             list = hcp.list("https://career.habr.com/vacancies/java_developer?page=");
             for (Post post : list) {
@@ -113,7 +110,8 @@ public class PsqlStore implements Store {
             System.out.println("**************************************************************");
             list = psql.getAll();
             list.stream().forEach(System.out::println);
-        } catch (IOException e) {
+        }
+        } catch (Exception e) {
             throw new RuntimeException(e);
         }
     }
